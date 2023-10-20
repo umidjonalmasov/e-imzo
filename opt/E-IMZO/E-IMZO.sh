780ace1585f1f7a56e6ade54571b3bc0cb9e218a
Umidjon Almasov <umidjon@almasov.uz>
2023-10-20 17:35:38 +0500

    Add files via upload

---
+++ opt/E-IMZO/E-IMZO.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# check if MacOS
+if [ -d /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin ]; then
+	ln -sf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java ./java
+	./java -jar E-IMZO.jar
+else
+        cd `dirname $0`
+	java -jar E-IMZO.jar
+fi
+
+exit 0
+
